e728d63121212e8932effd9c4e1291b2f386a8f4
LAPTOP-GVOQBMQD\Movahedians <Mov.mahdi@gmail.com>
2026-06-06 13:38:07 +0330

    Cargo.toml fix

---
+++ example_crate/c.rs
@@ -0,0 +1,63 @@
+fn deletion_demo(a: bool, b: bool) -> bool {
+    (a && b) || (!a && !b)
+}
+#[cfg(test)]
+#[allow(unused)]
+mod deletion_demo_test {
+    use super::*;
+    use mutantor::rand;
+    use mutantor::Mutable;
+    #[test]
+    fn test() {
+        let mut score = 0f64;
+        let mut rng = rand::rng();
+        let mut a = bool::new_mutable(&mut rng);
+        let mut b = bool::new_mutable(&mut rng);
+        if (deletion_demo(a.clone_mutable(), b.clone_mutable())
+            == deletion_demo_0(a.clone_mutable(), b.clone_mutable()))
+        {
+            println!("mutation {} survived _ {}", 0usize, " COD, LOD,");
+        } else {
+            println!("mutation {} killed _ {}", 0usize, " COD, LOD,");
+            score += 1f64;
+        }
+        if (deletion_demo(a.clone_mutable(), b.clone_mutable())
+            == deletion_demo_1(a.clone_mutable(), b.clone_mutable()))
+        {
+            println!("mutation {} survived _ {}", 1usize, " LOD, COD,");
+        } else {
+            println!("mutation {} killed _ {}", 1usize, " LOD, COD,");
+            score += 1f64;
+        }
+        if (deletion_demo(a.clone_mutable(), b.clone_mutable())
+            == deletion_demo_2(a.clone_mutable(), b.clone_mutable()))
+        {
+            println!("mutation {} survived _ {}", 2usize, " COD, COD,");
+        } else {
+            println!("mutation {} killed _ {}", 2usize, " COD, COD,");
+            score += 1f64;
+        }
+        if (deletion_demo(a.clone_mutable(), b.clone_mutable())
+            == deletion_demo_3(a.clone_mutable(), b.clone_mutable()))
+        {
+            println!("mutation {} survived _ {}", 3usize, " LOD, LOD,");
+        } else {
+            println!("mutation {} killed _ {}", 3usize, " LOD, LOD,");
+            score += 1f64;
+        }
+        println!("score {}", (score / 4f64) * 100f64);
+        assert!((score / 4f64) * 100f64 >= 100f64)
+    }
+    fn deletion_demo_0(a: bool, b: bool) -> bool {
+        (b && b) || (a && a)
+    }
+    fn deletion_demo_1(a: bool, b: bool) -> bool {
+        (b && b) || (!a && !a)
+    }
+    fn deletion_demo_2(a: bool, b: bool) -> bool {
+        (b && b) || (!a && !a)
+    }
+    fn deletion_demo_3(a: bool, b: bool) -> bool {
+        (a && b) || (!a && !b)
+    }
+}
--- example_crate/src/main.rs
+++ example_crate/src/main.rs
@@ -1,12 +1,12 @@
-use mutantor::generate_mutants;
+use mutantor::*;
+
 
 fn main() {
     println!("Hello, world!");
 }
 
-#[generate_mutants(IPVR, IPEX, IMCD,AOR,ROR,COR,LOR,SOR, path = "test.rs" , acc = 100.0, c_count = 2,m_count = 2)]
+#[generate_mutants(IPVR, IPEX, IMCD,AOR,ROR,COR,LOR,SOR,SDL, path = "a.rs" , acc = 100.0, c_count = 2,m_count = 2)]
 fn mutation_demo(a: i32, b: i32, c: bool, d: bool) -> i32 {
-    // AOR
     let mut x = a + b;
     x = x - 1;
     x = x * 2;
@@ -14,24 +14,18 @@ fn mutation_demo(a: i32, b: i32, c: bool, d: bool) -> i32 {
     x = x % 7;
 
 
-
-    // ROR + COR
     if (x > 10 && c) || (x <= 100 && d) {
         x += 1;
     }
 
-    // LOR
     let flags = (a & b) | (a ^ b);
 
-    // SOR
     let shifted = (flags << 2) >> 1;
 
 
-    // SDL opportunities
     let tmp1 = shifted + 1;
     let tmp2 = shifted - 1;
 
-    // More COR/ROR
     if x == tmp1 || x != tmp2 {
         x += 5;
     }
@@ -41,4 +35,18 @@ fn mutation_demo(a: i32, b: i32, c: bool, d: bool) -> i32 {
     }
 
     x
+}
+#[generate_mutants(AOI, COI, LOI, path = "b.rs" , acc = 100.0, c_count = 2,m_count = 2)]
+fn insertion_demo(a: i32, b: bool) -> i32 {
+    let x = a;
+
+    if b {
+        1
+    } else {
+        0
+    }
+}
+#[generate_mutants(COD,LOD, path = "c.rs" , acc = 100.0, c_count = 2,m_count = 2)]
+fn deletion_demo(a: bool, b: bool) -> bool {
+    (a && b) || (!a && !b)
 }
